d5011293b95b4dcc0a029b5d0223babaa46373fb
pallavisai23 <54880008+pallavisai23@users.noreply.github.com>
2020-07-22 10:35:01 +0530

    Add files via upload

---
+++ DesignPatterns/src/structural/facadeDesignPattern/Rectangle.java
@@ -0,0 +1,11 @@
+package structural.facadeDesignPattern;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("This is rectangle");
+	}
+
+}
